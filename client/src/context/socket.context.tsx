a44524ba00f175e4176046acfb4f9437fbca11db
Nikki Mae Evers <37475750+nikkimae@users.noreply.github.com>
2024-08-30 19:28:21 +0200

    Add Rooms logic to Context

--- client/src/context/socket.context.tsx
+++ client/src/context/socket.context.tsx
@@ -2,29 +2,38 @@ import { createContext, useContext, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 import { SOCKET_URL } from '../config/default';
 
-// Define an interface to describe the context shape
+
+interface Room {
+  id: string;
+  name: string;
+}
+
 interface Context {
   socket: Socket;
   username?: string;
   setUsername: (value?: string) => void;
+  roomId?: string;
+  rooms: Room[];
 }
-
 // Initialize a connection with the backend socket server
 export const socket = io(SOCKET_URL);
 
 // Create a context to store the socket instance and other values
 export const SocketContext = createContext<Context>({
   socket,
-  setUsername: () => {} // Provide a default no-op function to match the expected type
+  setUsername: () => {}, // Provide a default no-op function to match the expected type
+  rooms: [{id: "", name: ""}]
 });
 
 // Provider component to wrap around the app and provide the socket instance to all child components
 function SocketsProvider({ children }: { children: React.ReactNode }) {
   const [username, setUsername] = useState<string | undefined>("");
+  const [roomId, setRoomId] = useState<string | undefined>("");
+  const [rooms, setRooms] = useState<Room[]>([{ id: "", name: "" }]);
 
   return (
     // Pass the socket, username, and setUsername through the context provider
-    <SocketContext.Provider value={{ socket, username, setUsername }}>
+    <SocketContext.Provider value={{ socket, username, setUsername, rooms, roomId }}>
       {children} {/* Render any child components passed to the provider */}
     </SocketContext.Provider>
   );
